2518d258f8cfa91fb741e19c4d05ba64524dcec6
mkunwor <Kunwormanita979@gmail.com>
2023-01-12 11:38:42 +0545

    Visualization of timeseries data

--- app.py
+++ app.py
@@ -29,3 +29,25 @@ if df is not None:
     m=Prophet()
     m.fit(data)
 
+if df is not None:
+    future=m.make_future_dataframe(periods=periods_input)
+    
+    forecast = m.predict(future)
+    fcst=forecast[['ds','yhat','yhat_lower','yhat_upper']]
+
+    fcst_filtered=fcst[fcst['ds']>max_date]
+    st.write(fcst_filtered)
+
+    fig1=m.plot(forecast)
+    st.write(fig1)
+
+    fig2=m.plot_components(forecast)
+    st.write(fig2)
+
+# if df is not None:
+#     csv_exp=fcst_filtered.to_csv(index=False)
+#     #When no file name is given, pandas returns the csv as a string,nice.
+#     b64=base64.b64decode(csv_exp.encode()).decode()
+#     href=f'<a href="data:file/csv ;base64,{b64}">Download CSV File </a>(right- click and save as **&lt;forecast_name&gt;.csv**)'
+#     st.markdown(href,unsafe_allow_html=True)
+    
